33b66f0e0779b9c09ce98b2ed1058d61f12e8b1a
nickdys <nwd5069@psu.edu>
2012-10-07 23:23:41 -0400

    Filling User class

--- User.java
+++ User.java
@@ -1,5 +1,36 @@
+import java.util.Calendar;
+import java.util.Date;
 
 // TODO: Fill this class with meaningful content (right now it's just a placeholder)
 public class User {
-    public String name;
+    private String name;
+    private String password;
+    private Event[] events;         // TODO make this a list?
+    private int[] availability;
+    
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+        availablilty = // TODO
+    }
+    
+    public void addEvent(Event newEvent) {
+        events.add(newEvent);
+    }
+    
+    public void deleteEvent(Event deleteEvent) {
+        events.delete(deleteEvent);
+    }
+    
+    public Event[] getEvents() {
+        return events;
+    }
+    
+    public void setAvailability() {
+        // TODO
+    }
+    
+    public Time[] getAvailability() {
+        return availability;
+    }
 }
